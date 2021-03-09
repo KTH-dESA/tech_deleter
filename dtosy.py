afea89e225646fcc415b08893585f68e56633776
HauHe <haukeh@kth.se>
2021-03-09 10:44:18 +0100

    dtosy.py contains a script that takes in a list of technologies from a csv and a datapackage. It produces a new datapackage removing the technologies listed in the csv from the datapackage.

---
+++ dtosy.py
@@ -0,0 +1,50 @@
+#Scritp to delete technologies from an osemosys datapackage
+#%% Packages to import
+import os
+import pandas as pd
+from shutil import copyfile
+#%% Read in datapackage
+def read_dp(path):
+#    path = '..' #for testing
+    #datafiles =list()
+    datafiles = next(os.walk(path+'data'))
+    dic = dict()
+    j = 0
+    for j in range(len(datafiles[2])):
+        dic[datafiles[2][j]] = pd.read_csv(path+'data/'+datafiles[2][j])
+    return dic
+#%% Function to delete technologies from dataframes
+def dp_new(dic,techs):
+    dp_new_path = 'dp_new'
+    try:
+        os.mkdir(dp_new_path)
+    except OSError:
+        print("Creation of the directory %s failed" % dp_new_path)
+    copyfile('datapackage.json',dp_new_path+'/datapackage.json')
+    dp_new_path = dp_new_path+'/data'
+    try:
+        os.mkdir(dp_new_path)
+    except OSError:
+        print("Creation of the directory %s failed" % dp_new_path)
+    for i in dic:
+        df = dic[i]
+        if i=='TECHNOLOGY.csv':
+            #dic[i] = dic[i]['VALUE'].astype('str')
+            m = df.VALUE.isin(techs)
+            df = df[~m]
+            df.to_csv(dp_new_path+'/'+i,index=False)
+        elif 'TECHNOLOGY' in df.columns:
+            m = df.TECHNOLOGY.isin(techs)
+            df = df[~m]
+            df.to_csv(dp_new_path+'/'+i,index=False)
+    return 
+#%% Main Function to execute the script
+def main(dp_path,tech_path):
+    dic_data = read_dp(dp_path)
+    t2d = pd.read_csv(tech_path)
+    p_n_dp = dp_new(dic_data,t2d.iloc[:,0])
+#%% Delete technology by executing script
+if __name__ == '__main__':
+    techs = 't2d.csv'
+    dp_path = ''
+    main(dp_path,techs)
